99aac326df80c3e2bfe37bfb71780ff93b0e622b
Kevin Ho <bornarkun@gmail.com>
2016-02-21 21:46:05 -0500

    Twitter Files

---
+++ Twitter/TweetsViewController.swift
@@ -0,0 +1,48 @@
+//
+//  TweetsViewController.swift
+//  Twitter
+//
+//  Created by Kevin Ho on 2/20/16.
+//  Copyright © 2016 KevinHo. All rights reserved.
+//
+
+import UIKit
+
+class TweetsViewController: UIViewController {
+
+    var tweets: [Tweet]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        TwitterClient.sharedInstance.homeTimeLine({ (tweets: [Tweet]) -> () in
+            self.tweets = tweets
+            
+            for tweet in tweets {
+                print (tweet.text)
+            }
+        }, failure: { (error: NSError) -> () in
+                print(error.localizedDescription)
+        })
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onLogoutButton(sender: AnyObject) {
+        TwitterClient.sharedInstance.logout()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
