3bf71bab3052882a2d16a8e8492ec656511c393c
bornarkun <bornarkun@gmail.com>
2016-02-24 21:22:14 -0500

    Added files via upload

---
+++ Twitter/Twitter/Twitter/TweetsViewController.swift
@@ -0,0 +1,82 @@
+//
+//  TweetsViewController.swift
+//  Twitter
+//
+//  Created by Kevin Ho on 2/20/16.
+//  Copyright © 2016 KevinHo. All rights reserved.
+//
+
+import UIKit
+
+class TweetsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tweets: [Tweet]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "Twitter"
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        self.tableView.estimatedRowHeight = 200
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+
+        TwitterClient.sharedInstance.homeTimeLine({ (tweets: [Tweet]) -> () in
+            self.tweets = tweets
+            
+            if (tweets == tweets) {
+                
+                self.tableView.reloadData()
+           }
+            
+        }, failure: { (error: NSError) -> () in
+                print(error.localizedDescription)
+        })
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if tweets != nil {
+            
+            return tweets.count //Deleted exclaimation mark
+            
+        } else {
+            
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("TwitterCell", forIndexPath: indexPath) as! TwitterCell
+        cell.tweet = tweets[indexPath.row]
+        
+        return cell
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onLogoutButton(sender: AnyObject) {
+        TwitterClient.sharedInstance.logout()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
